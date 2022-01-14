4d71029d18cddb534c8e71a002cd4757862b1b9d
Microwave-WYB <david_wyb2001@outlook.com>
2022-01-14 13:29:27 -0800

    Update blur.py

--- blur.py
+++ blur.py
@@ -1,53 +1,19 @@
 #!/bin/python3
 import imp
-from xml.dom.expatbuilder import parseFragmentString
 from PIL import Image, ImageFilter
 from os import system
 from os import popen
 from os import path
 import time
 
-BG_PATH = "/home/wyb/.cache/wallpaper.png"
-CACHE_DIR = "/home/wyb/.cache/bg_blur"
+BG_PATH = path.expanduser("~") + "/.cache/wallpaper.png"
+CACHE_DIR = path.expanduser("~") + "/.cache/bg_blur"
 
-"""
-    img = Image.open(BG_PATH)
-    img = img.filter(ImageFilter.GaussianBlur(radius=50))
-    img.save(CACHE_DIR + "/blur.png")
-
-while True:
-    # check for focused window
-    pid = popen("swaymsg -t get_tree | jq '.. | select(.type?) | select(.focused==true).pid'").read()
-    if pid == "null\n" and focused:
-        # sleep for 2 seconds to avoid confusion
-        time.sleep(2)
-    if pid == "null\n"  and focused:
-        # undo blur
-        print("Unsetting blur")
-        #system("swaymsg output '*' background " + CACHE_DIR + "/wallpaper.png fill")
-        swaybg_pid = popen("pidof swaybg").read()
-        system("swaybg -i " + CACHE_DIR + "/wallpaper.png -m fill &")
-        time.sleep(0.5)
-        system("kill " + swaybg_pid)
-        focused = False
-    elif pid != "null\n" and not focused:
-        # set blur
-        print("Setting blur")
-        #system("swaymsg output '*' background " + CACHE_DIR + "/blur.png fill")
-        swaybg_pid = popen("pidof swaybg").read()
-
-        time.sleep(0.5)
-        system("kill " + swaybg_pid)
-        focused = True
-    else:
-        # do nothing
-        time.sleep(0.3)
-"""
+# Generate blurred background
 def gen_blur():
     print("[*] generating blurred backgrounds")
     global BG_PATH
     global CACHE_DIR
-    # generate blurred background files
     system("cp " + BG_PATH + " " + CACHE_DIR)
     img = Image.open(BG_PATH)
     for i in range(6):
@@ -55,6 +21,7 @@ def gen_blur():
         blur.save(CACHE_DIR + "/blur" + str(i) + ".png")
     print("[*] blurred backgrounds ready")
 
+# Prepare all swaybg processes to set the blurred background
 def prep_set_blur():
     global CACHE_DIR
     print("[*] preparing blur")
@@ -64,6 +31,7 @@ def prep_set_blur():
         time.sleep(0.05)
     print("[*] blurred background prepared")
 
+# Prepare all swaybg processes to unset the blurred background
 def prep_unset_blur():
     global CACHE_DIR
     print("[*] preparing unblur")
@@ -73,7 +41,8 @@ def prep_unset_blur():
         time.sleep(0.05)
     print("[*] unblurred background prepared")
 
-def change_blur():
+# Apply changes to swaybg processes
+def apply_change():
     print("[*] applying effects")
     # kill 5 swaybg processes from lower to higher pid
     pids = popen("pidof swaybg").read().split(" ")
@@ -91,27 +60,41 @@ def main():
     system("pkill swaybg")
     system("swaybg -i " + CACHE_DIR + "/wallpaper.png -m fill &")
     if not path.exists(BG_PATH):
-        print("[!] Error: No wallpaper found in ~/.cache/wallpaper")
+        print("[!] Error: No wallpaper found in", BG_PATH)
         print("try adding `output \"*\" bg /home/$USER/.cache/wallpaper fill` to your ~/.config/sway/config, and move your wallpaper there")
         exit(1)
-    if False: # TODO: Add cache check
+    if popen("diff " + BG_PATH + " " + CACHE_DIR + "/wallpaper.png").read() != "":
         # cache background
         system("cp " + BG_PATH + " " + CACHE_DIR)
-    gen_blur()
+        # generate blurred background
+        gen_blur()
+    # keep track of blurred state
     blurred = False
+    # prepare to set blurred background
     prep_set_blur()
+    # main loop
     while True:
+        # check if a window is focused
         focused_window = popen("swaymsg -t get_tree | jq '.. | select(.type?) | select(.focused==true).pid'").read()
+        if focused_window == "null\n" and blurred:
+            # wait a moment maybe a window will be focused in a sec
+            time.sleep(0.5)
+
+        # if a window is focused and blurred is false, apply blur
         if focused_window != "null\n" and not blurred:
-            change_blur()
+            apply_change()
+            # prepare to unset blurred background
             prep_unset_blur()
             blurred = True
+        # if a window is not focused and blurred is true, unblur
         elif focused_window == "null\n" and blurred:
-            change_blur()
+            apply_change()
+            # prepare to set blurred background
             prep_set_blur()
             blurred = False
         else:
             pass
-        time.sleep(1)
+        time.sleep(0.5)
+
 if __name__ == "__main__":
     main()
